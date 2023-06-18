2d8208e9f0078b8d5b7d340a62f2f79ef3ff8bb1
Domantas Martinaitis <domantas.martinaitis@gmail.com>
2023-06-18 23:17:08 +0200

    Basic project setup

---
+++ Parser/Source/Application.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Application.hpp"
+
+bool EntryPoint(const std::filesystem::path* const InputPath, const std::filesystem::path* const OutputPath)
+{
+	std::map<std::string, int> Data;
+
+
+
+	return true;
+}
